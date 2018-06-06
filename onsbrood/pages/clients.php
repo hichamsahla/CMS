d64932242bcf9a2c69f8f717c56fda5e3a31d77a
hichamsahla <23242@ma-web.nl>
2018-06-06 09:38:37 +0200

    Add files via upload

---
+++ onsbrood/pages/clients.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/main-style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="grid">
+    <div class="header">
+        <div class="slidebox">
+            <h3>select website</h3>
+            <select>
+                <option value="Ons Brood">Ons Brood</option>
+                <option value="Website 2">Website 2</option>
+                <option value="Website 3">Website 3</option>
+            </select>
+        </div>
+        <a href="#" class="button">Log Out</a>
+    </div>
+    <div class="menu">
+        <!--Menu-->
+        <div class="vertical-menu">
+            <a href="dashboard.php" class="active">Dashboard</a>
+            <a href="projects.php">Projects</a>
+            <a href="clients.php">Clients</a>
+            <a href="thehouse.php">The House</a>
+            <a href="neighbourhood.php">Neighbourhood</a>
+            <a href="contact.php">Contact</a>
+            <a href="cv.php">CV</a>
+            <a href="managing.php">Managing</a>
+            <a href="teacher.php">Teacher</a>
+        </div>
+    </div>
+    <div class="content">
+
+    </div>
+    <div class="footer">
+        <div class="copyright">&#169; CMS Rutger Kerstens </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ onsbrood/pages/dashboard.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/main-style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<div id="intoduction"
+ <div class="grid">
+     <div class="header">
+         <div class="slidebox">
+             <h3>select website</h3>
+         <select>
+         <option value="Ons Brood">Ons Brood</option>
+         <option value="Website 2">Website 2</option>
+         <option value="Website 3">Website 3</option>
+         </select>
+         </div>
+         <a href="#" class="button">Log Out</a>
+     </div>
+     <div class="menu">
+         <!--Menu-->
+         <div class="vertical-menu">
+             <a href="dashboard.php" class="active">Dashboard</a>
+             <a href="projects.php">Projects</a>
+             <a href="clients.php">Clients</a>
+             <a href="thehouse.php">The House</a>
+             <a href="neighbourhood.php">Neighbourhood</a>
+             <a href="contact.php">Contact</a>
+             <a href="cv.php">CV</a>
+             <a href="managing.php">Managing</a>
+             <a href="teacher.php">Teacher</a>
+         </div>
+     </div>
+     <div class="content">
+         <div class="title-content">
+             <h2>Welcome</h2>
+         </div>
+         <div class="dashboard">
+
+
+         </div>
+     </div>
+     <div class="footer">
+         <div class="copyright">&#169; CMS Rutger Kerstens </div>
+     </div>
+ </div>
+</body>
+</html>
